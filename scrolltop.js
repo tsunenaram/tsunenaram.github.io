391d008f2eaf89badbf8ef2c9addf3928910426f
tsunenaram <127706627+tsunenaram@users.noreply.github.com>
2023-03-24 10:55:31 +0900

    Update scrolltop.js

--- scrolltop.js
+++ scrolltop.js
@@ -1,18 +1,3 @@
-
-
-// デフォルトではボタンを非表示にする
-$(".scroll-top").hide();
-
-// 100px以上スクロールしたらボタンを表示させ、
-// それ以下の場合は非表示にする
-$(window).scroll(function() {
-  if($(this).scrollTop() > 100) {
-    $(".scroll-top").fadeIn();
-  } else {
-    $(".scroll-top").fadeOut();
-  }
-});
-
 // ボタンをクリックしたらページトップへ0.5秒で移動する
 $(".scroll-top").click(function() {
   $("html, body").animate({
